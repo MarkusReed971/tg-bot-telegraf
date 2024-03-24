fd8f47c3883e4cf49aecebf0697d3cdbee284182
Sidorenko Dmitriy <dsidorenko@i-novus.ru>
2024-03-24 20:36:25 +0300

    master Добавлена реализация через node-telegram-bot-api

--- index.ts
+++ index.ts
@@ -1,33 +1,84 @@
-import { Markup, Telegraf } from 'telegraf'
-import { message } from "telegraf/filters";
+// import { Markup, Telegraf } from 'telegraf'
+// import { message } from "telegraf/filters";
+
+import TelegramApi from 'node-telegram-bot-api'
+
 import * as process from 'process'
 import 'dotenv/config'
 
-const bot = new Telegraf(process.env.BOT_TOKEN as string)
+// ****************************** TELEGRAF ******************************
+
+// const bot = new Telegraf(process.env.BOT_TOKEN as string)
+// const clientUrl = process.env.CLIENT_URL as string
+//
+// bot.command('start', async (context) => {
+//   const { message } = context
+//
+//   return context.reply(
+//     `Привет, ${message.from.first_name}`,
+//     Markup.keyboard([
+//       Markup.button.webApp('Открыть сайт', clientUrl)
+//     ])
+//   )
+// })
+//
+// bot.on(message('web_app_data'), (context) => {
+//   const { message } = context
+//
+//   if (!message.web_app_data) {
+//     return
+//   }
+//
+//   const data = JSON.parse(message.web_app_data.data)
+//   const { country, city, street } = data
+//
+//   return context.reply(`Страна: ${country}, Город: ${city}, Улица: ${street}`, Markup.removeKeyboard())
+// })
+//
+// bot.launch()
+
+// ****************************** NODE-TELEGRAM-BOT-API ******************************
+
+const bot = new TelegramApi(process.env.BOT_TOKEN as string, {polling: true})
 const clientUrl = process.env.CLIENT_URL as string
 
-bot.command('start', async (context) => {
-  const { message } = context
-
-  return context.reply(
-    `Привет, ${message.from.first_name}`,
-    Markup.keyboard([
-      Markup.button.webApp('Открыть сайт', clientUrl)
+const start = async () => {
+    await bot.setMyCommands([
+        { command: '/start', description: 'Начать' }
     ])
-  )
-})
 
-bot.on(message('web_app_data'), (context) => {
-  const { message } = context
+    bot.on('message', async (message) => {
+        const { text, chat } = message
 
-  if (!message.web_app_data) {
-    return
-  }
+        if (text === '/start') {
+            return bot.sendMessage(chat.id, `Привет, ${chat.first_name}`, {
+                reply_markup: {
+                    keyboard: [
+                        [{ text: 'Открыть сайт', web_app: { url: clientUrl } }]
+                    ]
+                }
+            })
+        }
+    })
 
-  const data = JSON.parse(message.web_app_data.data)
-  const { country, city, street } = data
+    bot.on('web_app_data', async (message) => {
+        const { web_app_data, chat } = message
 
-  return context.reply(`Страна: ${country}, Город: ${city}, Улица: ${street}`, Markup.removeKeyboard())
-})
+        if (web_app_data) {
+            if (!web_app_data) {
+                return
+            }
 
-bot.launch()
+            const data = JSON.parse(web_app_data.data)
+            const { country, city, street } = data
+
+            return bot.sendMessage(chat.id, `Страна: ${country}, Город: ${city}, Улица: ${street}`, {
+                reply_markup: {
+                    remove_keyboard: true
+                }
+            })
+        }
+    })
+}
+
+start()
